5e4f40fdfb4a0deae7ee6a082188cb1cee191609
Bryan <bryanmihajatiana101@gmail.com>
2026-05-08 22:56:59 +0300

    fix: Update navbar links and restructure programme1 view for consistency

--- app/Views/programmes/programme1.php
+++ app/Views/programmes/programme1.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h2>programme 1</h2>
+<?php $this->extend('layout/main') ?>
+<?php $this->section('title'); ?>Programme de regimes<?php $this->endSection(); ?>
+<?php $this->section('content') ?>
+
+    <h2>Choisis ton programme</h2>
     <div class="container">
         <form action="<?= base_url('/suggerer') ?>" method="post">
             
@@ -66,5 +62,5 @@
             });
         });
     </script>
-</body>
-</html>
+
+<?php $this->endSection() ?>
