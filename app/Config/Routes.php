99e2db993ac10ae408d95ac9a2c3413add312f3f
Bryan <bryanmihajatiana101@gmail.com>
2026-05-08 14:57:13 +0300

    Add routes and view for adding money feature

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,13 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get(
+    '/code',
+    'CodeController::index'
+);
+
+$routes->post(
+    '/code/ajouter',
+    'CodeController::ajouterArgent'
+);
